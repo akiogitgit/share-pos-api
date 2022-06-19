8c13065d78d4b9447e224b80013fb6e7bfba3c92
Akio Tanaka <akimaron2828@icloud.com>
2022-06-19 15:54:04 +0900

    fix: user model, controller削除

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-  has_secure_token
-
-  # has_many :post, dependent: :destroy
-
-  validates :username, presence: true, uniqueness: true
-  validates :nickname, presence: true
-end
