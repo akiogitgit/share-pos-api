9a5a82ca2d663dad85a053cde29adf5b51b8efb5
Akio Tanaka <akimaron2828@icloud.com>
2022-08-10 15:01:58 +0900

    fix: column name

--- db/migrate/20220810052341_remove_devise_from_users.rb
+++ db/migrate/20220810052341_remove_devise_from_users.rb
@@ -3,13 +3,14 @@ class RemoveDeviseFromUsers < ActiveRecord::Migration[7.0]
     remove_column :users, :provider
     remove_column :users, :uid
     remove_column :users, :encrypted_password
-    remove_column :users, :reset_password_toke
-    remove_column :users, :reset_password_sent_a
+    remove_column :users, :reset_password_token
+    remove_column :users, :reset_password_sent_at
     remove_column :users, :allow_password_change
-    remove_column :users, :remember_created_a
-    remove_column :users, :confirmation_toke
-    remove_column :users, :confirmed_a
-    remove_column :users, :confirmation_sent_a
-    remove_column :users, :unconfirmed_emai
+    remove_column :users, :remember_created_at
+    remove_column :users, :confirmation_token
+    remove_column :users, :confirmed_at
+    remove_column :users, :confirmation_sent_at
+    remove_column :users, :unconfirmed_email
+    
   end
 end
