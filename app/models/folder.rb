9cdb4561dac9522374a7eec8dc149dcd5cdc65dc
Akio Tanaka <akimaron2828@icloud.com>
2022-08-15 13:43:07 +0900

    feat: folders#showのレスポンス修正

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -2,4 +2,6 @@ class Folder < ApplicationRecord
   belongs_to :user
   has_many :posts, through: :folder_post_relations
   has_many :folder_post_relations, dependent: :delete_all
+
+  validates :name, presence: true
 end
