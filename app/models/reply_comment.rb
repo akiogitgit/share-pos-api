6c39f0c6485674076a3ea52a7c6aec2627ac53d3
akiogitgit <akimaron2828@icloud.com>
2022-11-18 16:42:06 +0900

    feat: postを返すとき、reply_commentsも返す

--- app/models/reply_comment.rb
+++ app/models/reply_comment.rb
@@ -1,4 +1,12 @@
 class ReplyComment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  def as_json(options={})
+    super(
+      include: [
+        user: {only: [:id, :username]},
+      ]
+    )
+  end
 end
