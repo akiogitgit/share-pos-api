6ac779afdb8e3e6aa8da58c76965b1c38f7f8983
akiogitgit <akimaron2828@icloud.com>
2023-02-01 23:23:20 +0900

    chore: コミットのテスト

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_11_18_065526) do
+  # これいらない
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
     t.integer "user_id", null: false
