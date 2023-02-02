fd5e7f36b39c69edc544ff96c75602e636efbad8
akiogitgit <akimaron2828@icloud.com>
2023-02-02 15:15:36 +0900

    feat: #63 フォローのためのテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_18_065526) do
-  # これいらない
+ActiveRecord::Schema[7.0].define(version: 2023_02_02_013254) do
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
     t.integer "user_id", null: false
@@ -67,6 +66,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_18_065526) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_reply_comments_on_post_id"
     t.index ["user_id"], name: "index_reply_comments_on_user_id"
+  end
+
+  create_table "user_relations", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
